7b54ad65f9096ee48abba0b9a9ffe9401275560a
Slim Nate <slimnatekc@gmail.com>
2023-01-17 15:31:55 -0600

    refactor TitleHero into component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,45 +1,12 @@
 <script>
 	import Testimonials from '$lib/components/carousel/Testimonials.svelte';
+	import TitleHero from '$lib/components/TitleHero.svelte';
 </script>
 
 <!-- Section: Title Hero -->
-<section class="background-radial-gradient mb-32 text-center text-gray-800 lg:text-left">
-	<div
-		class="relative overflow-hidden bg-cover bg-no-repeat"
-		style="
-            background-position: 50%;
-            background-image: url('/images/placeholder_landscape.jpg');
-            height: 500px;
-          "
-	>
-		<div
-			class="absolute top-0 right-0 bottom-0 left-0 h-full w-full overflow-hidden bg-fixed"
-			style="background-color: rgba(0, 0, 0, 0.55)"
-		>
-			<div class="flex h-full items-center justify-center">
-				<div class="max-w-[800px] px-6 py-6 text-center text-white md:py-0 md:px-12">
-					<h2 class="mb-12 text-5xl font-bold leading-tight tracking-tight md:text-6xl xl:text-7xl">
-						Dream Capture Co
-					</h2>
-					<p class="text-lg">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima officia consequatur
-						adipisci tenetur repudiandae rerum quos.
-					</p>
-					<button
-						type="button"
-						class="mt-6 inline-block rounded-full border-2 border-gray-200 px-6 py-2
-					text-xs font-medium uppercase leading-tight text-gray-200
-					transition duration-150 ease-in-out hover:bg-black hover:bg-opacity-50 focus:outline-none focus:ring-0"
-					>
-						<a href="/booking">Book Now</a>
-					</button>
-				</div>
-			</div>
-		</div>
-	</div>
-</section>
-<!-- Section: Title Hero -->
+<TitleHero />
 
+<!-- Testimonials -->
 <Testimonials />
 
 <!-- Section: Carousel -->
