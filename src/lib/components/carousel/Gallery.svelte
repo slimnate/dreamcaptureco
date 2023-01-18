b4310d623b3bcf0bda815c1d562911e46b2d219c
Slim Nate <slimnatekc@gmail.com>
2023-01-18 15:41:19 -0600

    fix gallery bug

--- src/lib/components/carousel/Gallery.svelte
+++ src/lib/components/carousel/Gallery.svelte
@@ -89,7 +89,7 @@
 		<button
 			class="carousel-control-prev absolute top-0 bottom-0 left-0 flex items-center justify-center border-0 p-0 text-center hover:no-underline hover:outline-none focus:no-underline focus:outline-none"
 			type="button"
-			data-bs-target="#carouselExampleCaptions"
+			data-bs-target="#galleryCarousel"
 			data-bs-slide="prev"
 		>
 			<span class="carousel-control-prev-icon inline-block bg-no-repeat" aria-hidden="true" />
@@ -98,7 +98,7 @@
 		<button
 			class="carousel-control-next absolute top-0 bottom-0 right-0 flex items-center justify-center border-0 p-0 text-center hover:no-underline hover:outline-none focus:no-underline focus:outline-none"
 			type="button"
-			data-bs-target="#carouselExampleCaptions"
+			data-bs-target="#galleryCarousel"
 			data-bs-slide="next"
 		>
 			<span class="carousel-control-next-icon inline-block bg-no-repeat" aria-hidden="true" />
