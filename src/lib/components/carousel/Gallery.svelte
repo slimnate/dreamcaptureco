21240371f2fd486587d2892fcd03134ace6565d1
Slim Nate <slimnatekc@gmail.com>
2023-01-18 23:43:15 -0600

    Gallery now accepts arbitrary items list, supplied by parent component. Add button link to gallery slide

--- src/lib/components/carousel/Gallery.svelte
+++ src/lib/components/carousel/Gallery.svelte
@@ -1,24 +1,8 @@
 <script>
-	const slideItems = [
-		{
-			name: 'Slide 1',
-			description: 'Some representative placeholder content for the first slide.',
-			image: 'https://mdbootstrap.com/img/Photos/Slides/img%20(123).jpg',
-			link: '#!',
-		},
-		{
-			name: 'Slide 2',
-			description: 'Some representative placeholder content for the second slide.',
-			image: 'https://mdbootstrap.com/img/Photos/Slides/img%20(124).jpg',
-			link: '#!',
-		},
-		{
-			name: 'Slide 3',
-			description: 'Some representative placeholder content for the third slide.',
-			image: 'https://mdbootstrap.com/img/Photos/Slides/img%20(125).jpg',
-			link: '#!',
-		},
-	];
+	/**
+	 * @type {{name: string, description: string, image: string, link: string}[]}
+	 */
+	export let items = [];
 </script>
 
 <section class="mb-0 text-center">
@@ -26,7 +10,7 @@
 	<div id="galleryCarousel" class="slide carousel relative" data-bs-ride="carousel">
 		<div class="carousel-indicators absolute right-0 bottom-0 left-0 mb-4 flex justify-center p-0">
 			<!-- render carousel indicators -->
-			{#each slideItems as item, i}
+			{#each items as item, i}
 				<button
 					type="button"
 					data-bs-target="#galleryCarousel"
@@ -39,7 +23,7 @@
 		</div>
 		<div class="carousel-inner relative w-full overflow-hidden">
 			<!-- render carousel items -->
-			{#each slideItems as { name, description, image, link }, i}
+			{#each items as { name, description, image, link }, i}
 				<div class="carousel-item relative float-left w-full" class:active={i === 0}>
 					<div
 						class="relative overflow-hidden bg-cover bg-no-repeat"
@@ -53,6 +37,14 @@
 					<div class="carousel-caption absolute hidden text-center md:block">
 						<h5 class="text-xl">{name}</h5>
 						<p>{description}</p>
+						<button
+							type="button"
+							class="mt-2 inline-block rounded-full border-2 border-gray-200 px-6 py-2
+					text-xs font-medium uppercase leading-tight text-gray-200
+					transition duration-150 ease-in-out hover:bg-black hover:bg-opacity-50 focus:outline-none focus:ring-0"
+						>
+							<a href={link}>See More</a>
+						</button>
 					</div>
 				</div>
 			{/each}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,12 +3,33 @@
 	import Testimonials from '$lib/components/carousel/Testimonials.svelte';
 	import Footer from '$lib/components/Footer.svelte';
 	import TitleHero from '$lib/components/TitleHero.svelte';
+
+	const portfolioItems = [
+		{
+			name: 'Slide 1',
+			description: 'Some representative placeholder content for the first slide.',
+			image: 'https://mdbootstrap.com/img/Photos/Slides/img%20(123).jpg',
+			link: '#!',
+		},
+		{
+			name: 'Slide 2',
+			description: 'Some representative placeholder content for the second slide.',
+			image: 'https://mdbootstrap.com/img/Photos/Slides/img%20(124).jpg',
+			link: '#!',
+		},
+		{
+			name: 'Slide 3',
+			description: 'Some representative placeholder content for the third slide.',
+			image: 'https://mdbootstrap.com/img/Photos/Slides/img%20(125).jpg',
+			link: '#!',
+		},
+	];
 </script>
 
 <TitleHero />
 
 <Testimonials />
 
-<Gallery />
+<Gallery items={portfolioItems} />
 
 <Footer />
