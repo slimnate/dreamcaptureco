214744867228278363490db3825c981ff31ee089
Slim Nate <slimnatekc@gmail.com>
2023-01-21 01:32:52 -0600

    always center navbar logo on mobile layout, don't include it as part of dropdown menu

--- src/routes/Navbar.svelte
+++ src/routes/Navbar.svelte
@@ -17,11 +17,12 @@
 	];
 </script>
 
+<!--bg-gradient-to-b from-cream/70 via-cream to-cream/70-->
 <nav
 	class="
   navbar
-  navbar-light
   navbar-expand-md
+  navbar-light
 	relative
   flex
   w-full
@@ -31,17 +32,15 @@
   py-1 shadow-md
   "
 >
-	<div class="container-fluid flex w-full justify-between px-6">
+	<div class="container-fluid flex w-full flex-wrap justify-between px-6">
 		<!-- Toggler button -->
 		<button
 			class="
-      navbar-toggler
-      border-0
-      bg-transparent
-      py-2 px-2.5
-      hover:no-underline
-      hover:drop-shadow-md
-      focus:no-underline focus:shadow-none focus:outline-none focus:ring-0
+			navbar-toggler
+      w-[10vw]
+			border-0
+      bg-transparent py-2
+      pl-2.5
     "
 			type="button"
 			data-bs-toggle="collapse"
@@ -55,27 +54,24 @@
 		<!-- END Toggler button -->
 
 		<!-- Logo/Title TODO: move logo outside of nav collapse so it shows in center of navbar on mobile/tablet, not in dropdown -->
-		<a
-			class="
+		<div class="logo flex grow items-center justify-center md:justify-start">
+			<a
+				class="
         flex
         items-center
         lg:mt-0
       "
-			href="/"
-		>
-			<span class="semibold text-lg">Dream Capture</span>
-		</a>
-
-		<!-- navbar collapse -->
-		<div
-			class="navbar-collapse collapse flex-grow items-center border-t-2 border-black/80 md:border-none"
-			id="navbarSupportedContent"
-		>
+				href="/"
+			>
+				<span class="semibold text-lg">Dream Capture</span>
+			</a>
+		</div>
+		<div class="navbar-collapse collapse" id="navbarSupportedContent">
 			<!-- Page links -->
-			<ul class="list-style-none navbar-nav flex flex-grow flex-col justify-end">
+			<ul class="list-style-none navbar-nav flex flex-col md:justify-end">
 				{#each navItems as { href, text }}
 					<li class="nav-item p-2">
-						<a class="nav-link p-0 hover:drop-shadow-lg" {href}>{text}</a>
+						<a class="nav-link p-0  hover:drop-shadow-lg" {href}>{text}</a>
 					</li>
 				{/each}
 			</ul>
