cfe5731ea98bc141fae9d4bd8d3698ebb43076f7
Slim Nate <slimnatekc@gmail.com>
2023-01-22 11:02:55 -0600

    update site main and footer colors to fit theme

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -5,7 +5,6 @@
 	import IconInstagram from './icons/IconInstagram.svelte';
 	import IconMail from './icons/IconMail.svelte';
 	import IconPhone from './icons/IconPhone.svelte';
-	import FooterLink from './FooterLink.svelte';
 
 	const footerLinks = [
 		{ name: 'Wedding', href: '#/' },
@@ -19,31 +18,28 @@
 	];
 </script>
 
-<footer class="text-center text-gray-600 lg:text-left" style="background-color: beige;">
-	<div class="flex items-center justify-center border-b border-gray-300 p-6 md:justify-between">
+<footer class="text-center lg:text-left">
+	<div
+		class="flex items-center justify-center border-b border-xanadu p-4 md:justify-between md:p-6"
+	>
 		<div class="mr-12 hidden md:block">
 			<span>Get connected with us on social networks:</span>
 		</div>
 		<div class="flex justify-center">
 			<a
 				href="https://www.facebook.com/profile.php?id=100088847065471"
-				class="mr-6 text-gray-600"
+				class="mr-6"
 				target="_blank"
 				rel="noreferrer"
 			>
 				<IconFacebook />
 			</a>
-			<a
-				href="mailto:dream.capture.co.42@gmail.com"
-				class="mr-6 text-gray-600"
-				target="_blank"
-				rel="noreferrer"
-			>
+			<a href="mailto:dream.capture.co.42@gmail.com" class="mr-6" target="_blank" rel="noreferrer">
 				<IconGoogle />
 			</a>
 			<a
 				href="https://www.instagram.com/dream.capture.co/"
-				class="mr-6 text-gray-600"
+				class="mr-6"
 				target="_blank"
 				rel="noreferrer"
 			>
@@ -53,15 +49,11 @@
 	</div>
 	<div class="mx-6 py-6 text-center md:text-left">
 		<div class="grid-1 grid gap-8 md:grid-cols-3">
-			<div class="flex flex-col md:col-span-2 md:border-r-2 md:text-center">
+			<div class="flex flex-col border-blackcoffee md:col-span-2 md:border-r-2 md:text-center">
 				<h6 class="mb-4 flex justify-center font-semibold uppercase">Categories</h6>
-				<div class="grid-1 grid gap-4 md:grid-cols-2">
+				<div class="grid-1 grid gap-4 sm:grid-cols-2">
 					{#each footerLinks as { href, name }}
-						<a
-							{href}
-							class="text-gray-600 hover:scale-[1.02] hover:text-gray-800 hover:drop-shadow-lg"
-							>{name}</a
-						>
+						<a {href} class="hover:scale-[1.02] hover:drop-shadow-lg">{name}</a>
 					{/each}
 				</div>
 			</div>
@@ -82,8 +74,8 @@
 			</div>
 		</div>
 	</div>
-	<div class="bg-gray-200 p-6 text-center">
-		<span>© 2021 Copyright</span>
+	<div class="p-4 text-center text-sm">
+		<span>© 2023 Copyright</span>
 		<a class="font-semibold text-gray-600" href="https://www.slimnate.com/">slimnate</a>
 	</div>
 </footer>
