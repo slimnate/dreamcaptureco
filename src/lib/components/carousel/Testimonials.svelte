67169e42db9e443324a992c5e55fb5b09bfe1a3e
Slim Nate <slimnatekc@gmail.com>
2023-01-22 21:45:11 -0600

    update testimonials

--- src/lib/components/carousel/Testimonials.svelte
+++ src/lib/components/carousel/Testimonials.svelte
@@ -3,29 +3,30 @@
 
 	const testimonials = [
 		{
-			name: 'Quinton',
-			location: 'Lawrence, KS',
-			text: 'Izzy takes her time, she is very professional, and has a unique way of interacting with her clients. She’s a fantastic photographer! I will continue going back to Izzy for all my shoots!',
-			image: '/images/testimonials/quinton-01.jpg',
-		},
-		{
-			name: 'Bri',
-			location: 'Lees Summit, MO',
-			text: 'Not only are Izzy’s photos beautiful, but she is incredible to work with. I’m always amazed at how quickly Izzy gets me comfortable in front of the camera, and how beautiful she makes me feel in her pictures!',
-			image: '/images/testimonials/bri-01.jpg',
-			fitTop: true,
-		},
-		{
 			name: 'Electra',
 			location: 'Kansas City, MO',
 			text: 'Izzy is one of the sweetest photographers I have met! She is so fun and creative to work with! She makes your ideas come to life! I would recommend her to anyone! I have nothing but wonderful things to say!',
 			image: '/images/testimonials/electra-02.jpg',
 		},
 		{
+			name: 'Bri',
+			location: 'Lees Summit, MO',
+			text: 'Not only are Izzy’s photos beautiful, but she is incredible to work with. I’m always amazed at how quickly Izzy gets me comfortable in front of the camera, and how beautiful she makes me feel in her pictures!',
+			image: '/images/testimonials/bri-02.jpg',
+			fitTop: true,
+		},
+		{
+			name: 'Quinton',
+			location: 'Lawrence, KS',
+			text: 'Izzy takes her time, she is very professional, and has a unique way of interacting with her clients. She’s a fantastic photographer! I will continue going back to Izzy for all my shoots!',
+			image: '/images/testimonials/quinton-01.jpg',
+		},
+		{
 			name: 'Dani',
 			location: 'Kansas City, MO',
 			text: 'Izzy was amazing! I had a toddler and a puppy for our photoshoot and Izzy made it SO breezy. My son absolutely adored her positivity and how welcoming she was. I adore all of the pictures she got of us off guard, she captures all the best moments & makes them timeless!',
-			image: '/images/testimonials/dani-01.jpg',
+			image: '/images/testimonials/dani-02.jpg',
+			fitTop: true,
 		},
 	];
 </script>
