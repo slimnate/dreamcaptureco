8b75acfe3bea3ed7b3968e81b95b3dfec0ca6bab
Slim Nate <slimnatekc@gmail.com>
2023-01-30 01:50:26 -0600

    add carousel for every category to portfolio page

---
+++ src/routes/portfolio/+page.svelte
@@ -0,0 +1,55 @@
+<script>
+	import Gallery from '$lib/components/carousel/Gallery.svelte';
+	import Spacer from '$lib/components/Spacer.svelte';
+
+	/**
+	 * Returns a function that is passed to Array.map to map a
+	 * list of image file names into a gallery item format
+	 *
+	 * @param {string} name
+	 */
+	function itemMapper(name) {
+		return (/** @type {string} */ image) => {
+			let lower = name.toLowerCase();
+			return { image: `images/${lower}/${image}`, link: `/portfolio/${lower}`, name };
+		};
+	}
+
+	const portraitItems = ['001.jpg', '002.jpg', '003.jpg'].map(itemMapper('Portrait'));
+
+	const boudoirItems = ['001.jpg', '002.jpg', '003.jpg'].map(itemMapper('Boudoir'));
+
+	const couplesItems = ['001.jpg', '002.jpg', '003.jpg'].map(itemMapper('Couples'));
+
+	const familyItems = ['001.jpg', '002.jpg', '003.jpg'].map(itemMapper('Family'));
+
+	const maternityItems = ['001.jpg', '002.jpg', '003.jpg'].map(itemMapper('Maternity'));
+
+	const newbornItems = ['001.jpg', '002.jpg', '003.jpg'].map(itemMapper('Newborn'));
+</script>
+
+<Spacer>Portfolio</Spacer>
+
+<Gallery items={portraitItems} id="portraitsGallery" />
+
+<Spacer />
+
+<Gallery items={boudoirItems} id="portraitsGallery" />
+
+<Spacer />
+
+<Gallery items={couplesItems} id="portraitsGallery" />
+
+<Spacer />
+
+<Gallery items={familyItems} id="portraitsGallery" />
+
+<Spacer />
+
+<Gallery items={maternityItems} id="portraitsGallery" />
+
+<Spacer />
+
+<Gallery items={newbornItems} id="portraitsGallery" />
+
+<Spacer />
