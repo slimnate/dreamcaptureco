cc4579e168b83fc429b0e3e03b08fcac59ad52e2
Slim Nate <slimnatekc@gmail.com>
2023-01-30 12:52:38 -0600

    add images to portfolio pages

--- src/routes/portfolio/[category]/+page.js
+++ src/routes/portfolio/[category]/+page.js
@@ -1,6 +1,12 @@
+import { getAllImagesFor } from '$lib/data/portfolio';
+
 /** @type {import('./$types').PageLoad} */
-export async function load( { params }) {
-  return {
-    category: params.category
-  }
+export async function load({ params }) {
+	const category = params.category;
+	const images = getAllImagesFor(category);
+  
+	return {
+		category,
+		images,
+	};
 }
--- src/routes/portfolio/[category]/+page.svelte
+++ src/routes/portfolio/[category]/+page.svelte
@@ -1,6 +1,14 @@
 <script>
+	import Spacer from '$lib/components/Spacer.svelte';
+
 	/** @type {import('./$types').PageData} */
 	export let data;
 </script>
 
-<div>Welcome {data.category}</div>
+<Spacer>{data.category}</Spacer>
+
+<div class="mx-10 columns-1 sm:mx-auto sm:max-w-[90vw] sm:columns-2 md:max-w-[80vw] md:columns-3">
+	{#each data.images as img}
+		<img src={img} alt={img} class="py-2" />
+	{/each}
+</div>
