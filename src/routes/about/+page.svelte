d4affdbb6c1bd2f731602fe2a1d601646112b40c
Slim Nate <slimnatekc@gmail.com>
2023-02-06 10:58:17 -0600

    add basic about page template

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,16 @@
+<section class="text-center">
+	<h1 class="pb-8">About</h1>
+	<div class="about mb-12">
+		<img
+			src="/images/placeholder_portrait.jpg"
+			alt="Izzy Stevens"
+			class="mx-auto h-[50vw] max-h-[300px] w-[50vw] max-w-[300px] rounded-full object-cover"
+		/>
+		<div class="mx-auto mt-6 text-xl leading-10">Izzy Stevens</div>
+		<div class="about-text mx-auto max-w-[60ch] tracking-wide opacity-80">
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus possimus minus unde
+			repellendus nihil nisi ipsam cupiditate. Ullam repellat eaque ex quae, sit veritatis eligendi
+			omnis praesentium deserunt tempora inventore.
+		</div>
+	</div>
+</section>
