7ca43f6b188c6da4a7b074fd4dc7da8b3c5a53c4
Slim Nate <slimnatekc@gmail.com>
2023-02-08 13:01:44 -0600

    add left/right padding to about text

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -7,7 +7,7 @@
 			class="mx-auto h-[50vw] max-h-[300px] w-[50vw] max-w-[300px] rounded-full object-cover"
 		/>
 		<div class="mx-auto mt-6 text-xl leading-10">Izzy Stevens</div>
-		<div class="about-text mx-auto max-w-[60ch] tracking-wide opacity-80">
+		<div class="about-text mx-auto max-w-[60ch] px-6 tracking-wide opacity-80">
 			Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus possimus minus unde
 			repellendus nihil nisi ipsam cupiditate. Ullam repellat eaque ex quae, sit veritatis eligendi
 			omnis praesentium deserunt tempora inventore.
