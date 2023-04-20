e4f049c05bf6f8e3fd674ed9afd7817826c8368b
Slim Nate <slimnatekc@gmail.com>
2023-04-20 11:44:39 -0500

    update testimonials gallery for new tw-elements config

--- src/lib/components/carousel/Testimonials.svelte
+++ src/lib/components/carousel/Testimonials.svelte
@@ -1,5 +1,12 @@
 <script>
+	import { onMount } from 'svelte';
 	import IconQuote from '../icons/IconQuote.svelte';
+
+	import { Carousel, initTE } from 'tw-elements';
+
+	onMount(() => {
+		initTE({ Carousel });
+	});
 
 	/** @type {import('$lib/data/testimonials').TestimonialItem[]}*/
 	export let items = [];
@@ -8,14 +15,19 @@
 <!-- Section: Testimonials -->
 <section class="text-center">
 	<h1 class="pb-4">Testimonials</h1>
-	<div
-		id="testimonialsCarousel"
-		class="slide carousel carousel-dark relative"
-		data-bs-ride="carousel"
-	>
-		<div class="carousel-inner relative w-full overflow-hidden">
+	<!-- Carousel -->
+	<div id="testimonialsCarousel" class="relative" data-te-carousel-init data-te-carousel-slide>
+		<!-- Carousel Items container -->
+		<div class="relative w-full overflow-hidden after:clear-both after:block after:content-['']">
 			{#each items as { name, location, text, image, fitTop }, i}
-				<div class="carousel-item relative float-left w-full" class:active={i === 0}>
+				{@const additionalProperties = i === 0 ? { 'data-te-carousel-active': true } : {}}
+				<!-- Item -->
+				<div
+					class="relative float-left -mr-[100%] w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
+					class:hidden={i !== 0}
+					data-te-carousel-item
+					{...additionalProperties}
+				>
 					<div class="flex flex-wrap justify-center">
 						<div
 							class="flex w-10/12 max-w-[35rem] flex-col justify-end px-3 sm:w-8/12 md:w-6/12 lg:justify-center"
@@ -45,22 +57,50 @@
 
 		<!-- carousel buttons-->
 		<button
-			class="carousel-control-prev absolute top-0 bottom-0 left-0 flex items-center justify-center border-0 p-0 text-center hover:no-underline hover:outline-none focus:no-underline focus:outline-none"
+			class="absolute bottom-0 left-0 top-0 z-[1] flex w-[15%] items-center justify-center border-0 bg-none p-0 text-center opacity-50 transition-opacity duration-150 ease-[cubic-bezier(0.25,0.1,0.25,1.0)] hover:no-underline hover:opacity-90 hover:outline-none focus:no-underline focus:opacity-90 focus:outline-none motion-reduce:transition-none"
 			type="button"
-			data-bs-target="#testimonialsCarousel"
-			data-bs-slide="prev"
+			data-te-target="#testimonialsCarousel"
+			data-te-slide="prev"
 		>
-			<span class="carousel-control-prev-icon inline-block bg-no-repeat" aria-hidden="true" />
-			<span class="visually-hidden">Previous</span>
+			<span class="carousel-control-prev-icon inline-block bg-no-repeat" aria-hidden="true"
+				><svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke-width="1.5"
+					stroke="currentColor"
+					class="h-6 w-6"
+				>
+					<path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+				</svg></span
+			>
+			<span
+				class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
+				>Previous</span
+			>
 		</button>
 		<button
-			class="carousel-control-next absolute top-0 bottom-0 right-0 flex items-center justify-center border-0 p-0 text-center hover:no-underline hover:outline-none focus:no-underline focus:outline-none"
+			class="absolute bottom-0 right-0 top-0 z-[1] flex w-[15%] items-center justify-center border-0 bg-none p-0 text-center opacity-50 transition-opacity duration-150 ease-[cubic-bezier(0.25,0.1,0.25,1.0)] hover:no-underline hover:opacity-90 hover:outline-none focus:no-underline focus:opacity-90 focus:outline-none motion-reduce:transition-none"
 			type="button"
-			data-bs-target="#testimonialsCarousel"
-			data-bs-slide="next"
+			data-te-target="#testimonialsCarousel"
+			data-te-slide="next"
 		>
-			<span class="carousel-control-next-icon inline-block bg-no-repeat" aria-hidden="true" />
-			<span class="visually-hidden">Next</span>
+			<span class="inline-block h-8 w-8">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke-width="1.5"
+					stroke="currentColor"
+					class="h-6 w-6"
+				>
+					<path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+				</svg>
+			</span>
+			<span
+				class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
+				>Next</span
+			>
 		</button>
 	</div>
 </section>
