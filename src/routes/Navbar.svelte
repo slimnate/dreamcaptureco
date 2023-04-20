afafa420f8f0a9c88f098256221b911019241980
Slim Nate <slimnatekc@gmail.com>
2023-04-20 12:44:26 -0500

    change navbar breakpoint from lg to md

--- src/routes/Navbar.svelte
+++ src/routes/Navbar.svelte
@@ -24,6 +24,10 @@
 			href: '/about',
 			text: 'About',
 		},
+		{
+			href: '/faq',
+			text: 'FAQ',
+		},
 	];
 
 	/**
@@ -39,7 +43,7 @@
 	<div class="my-auto flex w-full flex-wrap items-center justify-between px-3">
 		<!-- Hamburger button for mobile view -->
 		<button
-			class="block border-0 bg-transparent px-2 hover:no-underline hover:shadow-none focus:no-underline focus:shadow-none focus:outline-none focus:ring-0 lg:hidden"
+			class="block border-0 bg-transparent px-2 hover:no-underline hover:shadow-none focus:no-underline focus:shadow-none focus:outline-none focus:ring-0 md:hidden"
 			type="button"
 			data-te-collapse-init
 			data-te-target="#navbarContent"
@@ -51,26 +55,26 @@
 		</button>
 
 		<!-- Navbar title -->
-		<div class="flex grow items-center justify-center lg:justify-start">
+		<div class="flex grow items-center justify-center md:justify-start">
 			<a class="pr-2 text-center text-xl" href="/"><span class="semibold">Dream Capture</span></a>
 		</div>
 
 		<!-- Collapsible navbar container -->
 		<div
-			class="!visible hidden flex-grow basis-[100%] lg:mt-0 lg:!flex lg:basis-auto lg:justify-end"
+			class="!visible hidden flex-grow basis-[100%] md:mt-0 md:!flex md:basis-auto md:justify-end"
 			id="navbarContent"
 			data-te-collapse-item
 		>
 			<!-- Left links -->
 			<ul
-				class="list-style-none my-auto flex flex-col justify-end pl-0 lg:flex-row"
+				class="list-style-none my-auto flex flex-col justify-end pl-0 md:flex-row"
 				data-te-navbar-nav-ref
 			>
 				{#each navItems as { href, text }, index}
 					<li
 						class:mt-4={index === 0}
 						class:mb-0={index === navItems.length - 1}
-						class="my-2 lg:mt-0 lg:mb-0 lg:px-2 lg:pr-2"
+						class="my-2 md:mt-0 md:mb-0 md:px-2 md:pr-2"
 						data-te-nav-item-ref
 					>
 						<a
