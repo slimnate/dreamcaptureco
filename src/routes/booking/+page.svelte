b7505daf15f38ff462495a877d8712c38bc24686
Slim Nate <slimnatekc@gmail.com>
2023-04-24 10:25:03 -0500

    implement netlify-forms for booking form, add preferred contact method field

--- src/routes/booking/+page.svelte
+++ src/routes/booking/+page.svelte
@@ -15,9 +15,14 @@
 		Fill out the form below and we'll reach out to finalize the details of your session!
 	</p>
 
-	<form class="booking-form mx-auto grid gap-3 px-8 md:max-w-3xl md:grid-cols-2">
+	<form
+		name="booking"
+		method="POST"
+		data-netlify="true"
+		class="booking-form mx-auto grid gap-3 px-8 md:max-w-3xl md:grid-cols-2"
+	>
 		<!-- NAME -->
-		<div class="relative  md:col-span-2" data-te-input-wrapper-init>
+		<div class="relative md:col-span-2" data-te-input-wrapper-init>
 			<input
 				type="text"
 				class="peer-focus:text-primary dark:peer-focus:text-primary peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
@@ -59,6 +64,37 @@
 				class="peer-focus:text-primary dark:peer-focus:text-primary pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[1.15rem] peer-focus:scale-[0.8] peer-data-[te-input-state-active]:-translate-y-[1.15rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200"
 				>Email</label
 			>
+		</div>
+
+		<div class="relative flex flex-wrap justify-between gap-3 px-3 md:col-span-2">
+			<label for="prefferedContact" class="text-left text-neutral-500"
+				>Preferred Contact Method
+			</label>
+			<div class="flex gap-8">
+				<div class="mb-[0.125rem] block min-h-[1.5rem] pl-[1.5rem]">
+					<input
+						class="checked:border-primary checked:after:border-primary checked:after:bg-primary checked:focus:border-primary dark:checked:border-primary dark:checked:after:border-primary dark:checked:after:bg-primary dark:checked:focus:border-primary relative float-left -ml-[1.5rem] mr-1 mt-0.5 h-5 w-5 appearance-none rounded-full border-2 border-solid border-neutral-300 before:pointer-events-none before:absolute before:h-4 before:w-4 before:scale-0 before:rounded-full before:bg-transparent before:opacity-0 before:shadow-[0px_0px_0px_13px_transparent] before:content-[''] after:absolute after:z-[1] after:block after:h-4 after:w-4 after:rounded-full after:content-[''] checked:before:opacity-[0.16] checked:after:absolute checked:after:left-1/2 checked:after:top-1/2 checked:after:h-[0.625rem] checked:after:w-[0.625rem] checked:after:rounded-full checked:after:content-[''] checked:after:[transform:translate(-50%,-50%)] hover:cursor-pointer hover:before:opacity-[0.04] hover:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:shadow-none focus:outline-none focus:ring-0 focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] checked:focus:before:scale-100 checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] dark:border-neutral-600 dark:focus:before:shadow-[0px_0px_0px_13px_rgba(255,255,255,0.4)] dark:checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca]"
+						type="radio"
+						name="prefferedContact"
+						id="radioEmail"
+					/>
+					<label class="mt-px inline-block pl-[0.15rem] hover:cursor-pointer" for="radioEmail">
+						Email
+					</label>
+				</div>
+				<div class="mb-[0.125rem] block min-h-[1.5rem] pl-[1.5rem]">
+					<input
+						class="checked:border-primary checked:after:border-primary checked:after:bg-primary checked:focus:border-primary dark:checked:border-primary dark:checked:after:border-primary dark:checked:after:bg-primary dark:checked:focus:border-primary relative float-left -ml-[1.5rem] mr-1 mt-0.5 h-5 w-5 appearance-none rounded-full border-2 border-solid border-neutral-300 before:pointer-events-none before:absolute before:h-4 before:w-4 before:scale-0 before:rounded-full before:bg-transparent before:opacity-0 before:shadow-[0px_0px_0px_13px_transparent] before:content-[''] after:absolute after:z-[1] after:block after:h-4 after:w-4 after:rounded-full after:content-[''] checked:before:opacity-[0.16] checked:after:absolute checked:after:left-1/2 checked:after:top-1/2 checked:after:h-[0.625rem] checked:after:w-[0.625rem] checked:after:rounded-full checked:after:content-[''] checked:after:[transform:translate(-50%,-50%)] hover:cursor-pointer hover:before:opacity-[0.04] hover:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:shadow-none focus:outline-none focus:ring-0 focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] checked:focus:before:scale-100 checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] dark:border-neutral-600 dark:focus:before:shadow-[0px_0px_0px_13px_rgba(255,255,255,0.4)] dark:checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca]"
+						type="radio"
+						name="prefferedContact"
+						id="radioPhone"
+						checked
+					/>
+					<label class="mt-px inline-block pl-[0.15rem] hover:cursor-pointer" for="radioPhone">
+						Phone
+					</label>
+				</div>
+			</div>
 		</div>
 
 		<!-- PACKAGE TYPE -->
