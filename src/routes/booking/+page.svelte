18a9a931c858c1bbfd410461c6b5bccc9a5cd2d0
Slim Nate <slimnatekc@gmail.com>
2023-04-24 10:35:16 -0500

    change button type to submit

--- src/routes/booking/+page.svelte
+++ src/routes/booking/+page.svelte
@@ -214,7 +214,7 @@
 
 		<!-- SUBMIT BUTTON -->
 		<button
-			type="button"
+			type="submit"
 			class="bg-primary hover:bg-primary-600 focus:bg-primary-600 active:bg-primary-700 mx-auto mb-12 mt-4 block min-w-[90%] rounded px-7 pb-2.5 pt-3 text-sm font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] md:col-span-2 md:min-w-[50%]"
 		>
 			Submit
