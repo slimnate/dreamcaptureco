4dd697944c9c6a72d1fd57d14e80babc7048d303
Slim Nate <slimnatekc@gmail.com>
2023-05-04 14:17:16 -0500

    add add-ons section to pricing page

--- src/routes/pricing/+page.svelte
+++ src/routes/pricing/+page.svelte
@@ -42,14 +42,39 @@
 			addOns: ['+$50 shoot with snakes'],
 		},
 	];
+
+	const addOns = [
+		{
+			name: 'Snakes',
+			description: 'Shoot with two ball pythons, Drakho and Nina',
+			price: '$50',
+		},
+		{
+			name: 'Fast turnaround',
+			description: 'Get your pictures a week early with expedited delivery',
+			price: '$50',
+		},
+		{
+			name: 'Extra images',
+			description: 'Get extra edited images',
+			price: '$50',
+		},
+		{
+			name: 'Physical delivery',
+			description: 'Get a physical copy of your images on a flash drive',
+			price: '$50',
+		},
+	];
 </script>
 
 <section class="pb-12">
+	<!-- Pricing header -->
 	<div id="pricing" class="background-radial-gradient text-center text-eggshell">
 		<h2 class="mb-12 text-center text-5xl font-bold">Pricing</h2>
 	</div>
 
-	<div class="grid px-6 md:grid-cols-3 md:px-12 xl:px-32" style="margin-top: -200px">
+	<!-- Pricing content -->
+	<div class="mb-8 grid px-6 md:grid-cols-3 md:px-12 xl:px-32" style="margin-top: -200px">
 		{#each tiers as tier, index}
 			<!-- Price block -->
 			<div class:py-12={index !== 1} class="p-0">
@@ -94,6 +119,24 @@
 						</ol>
 					</div>
 				</div>
+			</div>
+		{/each}
+	</div>
+
+	<!-- Add ons -->
+	<span class="bold m-4 text-3xl font-bold text-blackcoffee">Add-Ons</span>
+	<div class="grid gap-3 p-4 md:grid-cols-2 md:flex-row md:gap-4 md:px-12 xl:grid-cols-4 xl:px-32">
+		{#each addOns as { name, description, price }}
+			<div
+				class="flex grow flex-col justify-between gap-2 rounded-lg bg-eggshell p-4 shadow-lg sm:!p-8 md:gap-4 xl:!p-4"
+			>
+				<div
+					class="flex flex-row justify-between gap-2 text-left md:!flex-col md:items-center md:!text-center"
+				>
+					<span class="text-xl font-bold uppercase">{name}</span>
+					<span class="font-bold text-blackcoffee/70">{price}</span>
+				</div>
+				<span class="font-small text-blackcoffee/70">{description}</span>
 			</div>
 		{/each}
 	</div>
