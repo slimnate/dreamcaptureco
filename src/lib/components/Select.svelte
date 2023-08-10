90140bd53a6f2461f429717b90fd1821f5fd2c4b
SlimNate <slimnatekc@gmail.com>
2023-08-10 06:47:04 -0500

    Implement custom Select component with material styling to match the rest of the form

--- src/lib/components/Select.svelte
+++ src/lib/components/Select.svelte
@@ -1,46 +1,4 @@
 <script>
-	import { onMount } from 'svelte';
-
-	onMount(() => {
-		function optionItemClick(e) {
-			// TODO
-		}
-
-		function closeAllSelect(element) {}
-
-		const selectContainer = document.getElementsByClassName('custom-select')[0];
-		const selectElement = selectContainer.getElementsByTagName('select')[0];
-		const itemCount = selectElement.length;
-
-		// Create and display div to show current selected element
-		const selectedItemDiv = document.createElement('div');
-		selectedItemDiv.setAttribute('class', 'select-selected');
-		console.log(selectElement);
-		selectedItemDiv.innerHTML = selectElement.options[selectElement.selectedIndex].innerHTML;
-		selectContainer.appendChild(selectedItemDiv);
-
-		// Create options list
-		const optionList = document.createElement('div');
-		optionList.setAttribute('class', 'select-items select-hide');
-
-		// populate options list with items
-		for (let i = 0; i < itemCount; i++) {
-			const optionItem = document.createElement('div');
-			optionItem.innerHTML = selectElement.options[i].innerHTML;
-			optionItem.addEventListener('click', optionItemClick);
-			optionList.appendChild(optionItem);
-		}
-
-		// add option list to DOM
-		selectContainer.appendChild(optionList);
-		selectedItemDiv.addEventListener('click', function (e) {
-			e.stopPropagation();
-			closeAllSelect(this);
-			this.nextSibling.classList.toggle('select-hide');
-			this.classList.toggle('select-arrow-active');
-		});
-	});
-
 	/**
 	 * @typedef {Object} SelectOption
 	 * @property {string} value
@@ -48,103 +6,153 @@
 	 * @property {string?} secondary
 	 */
 
+	/**
+	 * Custom action that detects a mouse click anywhere outside the element to
+	 * which it was applied, and fires a 'click_outside' event.
+	 * @param {HTMLElement} node
+	 */
+	function clickOutside(node) {
+		/**
+		 * Event handle for click events on the DOM. Detects if a click was within
+		 * the parent node, and fires an event if not.
+		 * @param {MouseEvent} event */
+		const handleClick = (event) => {
+			if (node && !node.contains(event.target) && !event.defaultPrevented) {
+				node.dispatchEvent(new CustomEvent('click_outside', node));
+			}
+		};
+
+		document.addEventListener('click', handleClick);
+
+		return {
+			destroy() {
+				document.removeEventListener('click', handleClick, true);
+			},
+		};
+	}
+
+	function toggleOptionsShown() {
+		isOpen = !isOpen;
+	}
+
+	function closeOptions() {
+		isOpen = false;
+	}
+
+	let isOpen = false;
+	let selectedValue = '';
+
 	/** @type SelectOption[] */
 	export let options = [];
 	export let id = 'select';
-	let selectedItemIndex = 0;
+	export let placeholder = 'Select a value:';
+
+	$: isPlaceholderShown = selectedValue === '';
 </script>
 
-<div class="relative">
-	<select
-		{id}
-		name={id}
-		class="peer select"
-		data-selected=""
-		on:change={(e) => {
-			e.currentTarget.dataset.selected = e.currentTarget.value;
-			console.log(e);
-		}}
-		on:click={(e) => {
-			if (e.currentTarget.dataset.open == 'true') e.currentTarget.dataset.open = 'true';
-			else e.currentTarget.dataset.open = 'false';
-		}}
-	>
+<div class="relative" use:clickOutside on:click_outside={closeOptions}>
+	<select {id} name={id} value={selectedValue}>
+		<option value="" />
 		{#each options as option}
 			<option value={option.value}>{option.display}</option>
 		{/each}
 	</select>
-	<label for="session" class="floating-select">Session Type</label>
-</div>
 
-<!-- .custom-select select {
-		display: none; /*hide original SELECT element:*/
-	} -->
+	<div
+		class="select"
+		class:open={isOpen}
+		class:selected={!isPlaceholderShown}
+		on:click={toggleOptionsShown}
+		tabindex="0"
+	>
+		{isPlaceholderShown ? placeholder : selectedValue}
+	</div>
+
+	<div class="options" class:hide={!isOpen}>
+		{#each options as option}
+			<div
+				on:click={() => {
+					selectedValue = option.value;
+					toggleOptionsShown();
+				}}
+			>
+				{option.display}
+			</div>
+		{/each}
+	</div>
+
+	<label for="session" class:floating={!isPlaceholderShown}>{placeholder}</label>
+</div>
 
 <style lang="postcss">
 	select {
-		@apply variant-form-material border-blackcoffee-300 pb-1 pl-4 pt-3 focus:border-blackcoffee-500;
+		display: none;
 	}
 
-	label.floating-select {
-		@apply absolute left-4 top-0 text-xs font-semibold text-blackcoffee-500/70 transition-all duration-200 peer-placeholder-shown:top-2.5 peer-placeholder-shown:text-base peer-placeholder-shown:font-normal peer-placeholder-shown:text-blackcoffee-500 peer-[[data-selected=""]]:left-4 peer-[[data-selected=""]]:top-2 peer-[[data-selected=""]]:text-base peer-[[data-selected=""]]:font-normal peer-[[data-selected=""]]:text-blackcoffee-500/60;
+	.select {
+		@apply variant-form-material border-blackcoffee-300 pb-1 pl-4 pt-3 text-left focus:border-blackcoffee-500;
 	}
 
-	/*the container must be positioned relative:*/
-	.custom-select {
-		position: relative;
-		font-family: Arial;
+	.select.selected {
+		@apply text-primary-500;
 	}
 
-	.select-selected {
-		background-color: DodgerBlue;
+	.select:focus {
+		@apply border-blackcoffee-300 outline-none;
 	}
 
-	/*style the arrow inside the select element:*/
-	.select-selected:after {
+	/*chevron inside select box*/
+	.select:after {
+		@apply border-b-[0.175rem] border-r-[0.175rem] border-blackcoffee-300;
 		position: absolute;
+		right: 1rem;
 		content: '';
-		top: 14px;
-		right: 10px;
-		width: 0;
-		height: 0;
-		border: 6px solid transparent;
-		border-color: #fff transparent transparent transparent;
+		top: 1rem;
+		width: 0.5rem;
+		height: 0.5rem;
+		transform: rotate(45deg);
 	}
 
-	/*point the arrow upwards when the select box is open (active):*/
-	.select-selected.select-arrow-active:after {
-		border-color: transparent transparent #fff transparent;
-		top: 7px;
+	/*point the chevron upwards when options list shown:*/
+	.select.open:after {
+		transform: translateY(0.25rem) rotate(-135deg);
 	}
 
-	/*style the items (options), including the selected item:*/
-	.select-items div,
-	.select-selected {
-		color: #ffffff;
-		padding: 8px 16px;
-		border: 1px solid transparent;
-		border-color: transparent transparent rgba(0, 0, 0, 0.1) transparent;
-		cursor: pointer;
+	/*prevent select of options including current selected*/
+	.options div,
+	.select {
 		user-select: none;
 	}
 
-	/*style items (options):*/
-	.select-items {
+	.hide {
+		display: none;
+	}
+	.options {
 		position: absolute;
-		background-color: DodgerBlue;
 		top: 100%;
 		left: 0;
 		right: 0;
 		z-index: 99;
+		@apply rounded-b-md border-[1px] border-t-0 border-blackcoffee-300 bg-surface-400;
 	}
 
-	/*hide the items when the select box is closed:*/
-	.select-hide {
-		display: none;
+	.options div {
+		@apply border-b-[1px] border-blackcoffee-300 py-2;
 	}
 
-	.select-items div:hover,
-	.same-as-selected {
-		background-color: rgba(0, 0, 0, 0.1);
+	.options div:last-child {
+		@apply rounded-b-md border-none;
+	}
+
+	.options div:hover {
+		@apply bg-surface-500;
+	}
+
+	label.floating {
+		@apply left-4 top-0 text-xs font-semibold text-blackcoffee-500/70 transition-all duration-200;
+	}
+
+	label {
+		@apply absolute left-4 top-3 text-base font-normal text-transparent;
 	}
 </style>
