171445f8000c6d3e592331de93ea91b2adabdabc
SlimNate <slimnatekc@gmail.com>
2023-08-22 16:18:18 -0500

    add ModalImagePresenter component with support for navigating through the images in the presenter directly with arrow keys

---
+++ src/lib/components/ModalImagePresenter.svelte
@@ -0,0 +1,94 @@
+<script>
+	import { createEventDispatcher } from 'svelte';
+
+	const dispatch = createEventDispatcher();
+
+	/**
+	 * Hide the presenter and return focus back to the previousFocusedElement.
+	 */
+	function hide() {
+		// remove event listeners
+		document.removeEventListener('keydown', handleKeys);
+		shown = false;
+		dispatch('hide', currentIndex);
+	}
+
+	/**
+	 * Show the presenter
+	 * @param index {Number | null | undefined}
+	 * @param returnFocusTo { HTMLElement | null }
+	 */
+	export function show(index) {
+		// add event listeners
+		document.addEventListener('keydown', handleKeys);
+
+		currentIndex = index || 0;
+		shown = true;
+	}
+
+	function nextImage() {
+		if (currentIndex === images.length - 1) {
+			currentIndex = 0;
+		} else {
+			currentIndex++;
+		}
+	}
+
+	function previousImage() {
+		if (currentIndex === 0) {
+			currentIndex = images.length - 1;
+		} else {
+			currentIndex--;
+		}
+	}
+
+	/**
+	 * Handle keyboard events on the open presenter:
+	 * - intercept tab to keep focus on modal
+	 * - close on Escape, Enter, or space keys
+	 * @param {KeyboardEvent} event
+	 */
+	function handleKeys(event) {
+		const key = event.key;
+
+		console.log(key);
+		// capture tab
+		if (key === 'Tab') event.preventDefault();
+
+		if (key == 'ArrowLeft') {
+			previousImage();
+		}
+
+		if (key == 'ArrowRight') {
+			nextImage();
+		}
+
+		if (key === 'Escape' || key === 'Enter' || key === ' ') {
+			hide();
+			event.preventDefault(); // prevent default to keep space from reopening after focus is returned to calling element.
+		}
+	}
+
+	let /** @type {Boolean} */ shown = false;
+	let /** @type {Number} */ currentIndex = 0;
+	let /** @type {HTMLElement | null} */ previousFocusedElement;
+
+	export let /** @type {string[]} */ images;
+</script>
+
+<div class="fixed inset-0 z-10 bg-blackcoffee/50" class:hidden={!shown}>
+	<button class=" h-full w-full" on:click={hide}>
+		<div class="relative flex items-center justify-center">
+			<img
+				class="object-fit max-h-[100vh] max-w-[100vw]"
+				alt="Full screen"
+				src={images[currentIndex]}
+			/>
+			<div
+				class="absolute top-0 rounded-b-lg bg-eggshell/30 px-4 py-1 text-blackcoffee/70 backdrop-blur-md"
+			>
+				Click anywhere to close the preview
+			</div>
+		</div>
+	</button>
+</div>
--- src/routes/portfolio/[category]/+page.svelte
+++ src/routes/portfolio/[category]/+page.svelte
@@ -1,69 +1,38 @@
 <script>
+	import ModalImagePresenter from '$lib/components/ModalImagePresenter.svelte';
 	import { capitalize } from '$lib/string';
 
 	/** @type {import('./$types').PageServerData} */
 	export let data;
 
-	/** @type {HTMLElement | null} */
-	let focusedElementBeforePresenter;
-
-	/** @type {HTMLButtonElement | null} */
-	let presenterElement;
-
-	/** @type {HTMLImageElement | null} */
-	let presenterImageElement;
-
-	/**
-	 * Hide the presenter and return focus back to the previously focused element.
-	 */
-	function hidePresenter() {
-		presenterElement?.classList.add('hidden');
-		focusedElementBeforePresenter?.focus();
-	}
-
-	/**
-	 * Show the presenter with the specified image source
-	 * @param {string} source
-	 */
-	function showPresenter(source) {
-		presenterImageElement?.setAttribute('src', source);
-		presenterElement?.classList.remove('hidden');
-		presenterElement?.addEventListener('keydown', handleKeys);
-		presenterElement?.focus();
-	}
-
-	/**
-	 * Handle keyboard events on the open presenter:
-	 * - intercept tab to keep focus on modal
-	 * - close on Escape, Enter, or space keys
-	 * @param {KeyboardEvent} event
-	 */
-	function handleKeys(event) {
-		const key = event.key;
-
-		console.log(key);
-		// handle tab key press
-		if (key === 'Tab') event.preventDefault();
-
-		//close modal
-		if (key === 'Escape' || key === 'Enter' || key === ' ') hidePresenter();
-	}
+	let /** @type {ModalImagePresenter} */ presenter;
 
 	/**
 	 * Handle click of a gallery image, will also be called when enter/space pressed
 	 * on focus for keyboard navigators/screenreaders.
-	 * @param {MouseEvent} event
+	 * @param {MouseEvent & {currentTarget: HTMLElement | null}} event
 	 */
 	function handleClick(event) {
-		// save reference to previously focused element before opening presenter
-		// @ts-ignore
-		focusedElementBeforePresenter = document.activeElement;
+		console.log(event);
+		const index = new Number(
+			event.currentTarget?.querySelector('img')?.getAttribute('data-index')
+		).valueOf();
 
-		// @ts-ignore
-		const imageElement = event.currentTarget?.querySelector('img');
-		const source = imageElement.getAttribute('src');
+		presenter.show(index);
+	}
 
-		showPresenter(source);
+	/**
+	 * Handle presenter 'hide' event to focus the image that was shown last
+	 * @param event {CustomEvent<number>}
+	 */
+	function handlePresenterHide(event) {
+		console.log('close');
+		console.log(event);
+
+		const currentIndex = event.detail;
+		const img = document.getElementById(`image-btn-${currentIndex}`);
+		console.log(img);
+		document.getElementById(`image-btn-${currentIndex}`)?.focus();
 	}
 </script>
 
@@ -77,9 +46,10 @@
 	<div
 		class="mx-10 columns-1 sm:!mx-auto sm:max-w-[90vw] sm:columns-2 md:max-w-[80vw] md:columns-3"
 	>
-		{#each data.images as img}
-			<button type="button" on:click={handleClick} class="presenter-show">
+		{#each data.images as img, i}
+			<button id={`image-btn-${i}`} type="button" on:click={handleClick} class="presenter-show">
 				<img
+					data-index={i}
 					src={img}
 					alt={img}
 					class="py-2 drop-shadow-lg hover:scale-[101%] hover:brightness-90"
@@ -87,30 +57,5 @@
 			</button>
 		{/each}
 	</div>
-	<button
-		class="presenter hidden h-full w-full"
-		on:click={hidePresenter}
-		bind:this={presenterElement}
-	>
-		<div class="relative flex items-center justify-center">
-			<img
-				class="presenter-image object-fit max-h-[100vh] max-w-[100vw]"
-				id="presenter-image"
-				alt="Full screen"
-				bind:this={presenterImageElement}
-			/>
-			<div
-				class="absolute top-0 rounded-b-lg bg-eggshell/30 px-4 py-1 text-blackcoffee/70 backdrop-blur-md"
-			>
-				Click anywhere to close the preview
-			</div>
-		</div></button
-	>
+	<ModalImagePresenter images={data.images} bind:this={presenter} on:hide={handlePresenterHide} />
 </section>
-
-<style lang="postcss">
-	.presenter {
-		@apply inset-0 h-[100vh] w-[100vw] bg-black/50;
-		position: fixed;
-	}
-</style>
