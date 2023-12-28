cbe7852218c7364089ca5a9e57982f711330b80e
SlimNate <slimnatekc@gmail.com>
2023-12-27 18:03:37 -0600

    Add referral field, update field hints on textarea fields

--- src/lib/components/form-fields/TextArea.svelte
+++ src/lib/components/form-fields/TextArea.svelte
@@ -2,6 +2,7 @@
 	export let /** @type string */ id;
 	export let /** @type string */ name;
 	export let /** @type string */ label;
+	export let /** @type string */ hint = '';
 	export let /** @type string */ value = '';
 </script>
 
@@ -14,6 +15,7 @@
   - `id` **required** - id of the underlying `textarea` element
   - `name` **required** - name of the underlying `textarea` element
   - `label` **required** - placeholder and label text for field
+  - `hint` **optional** - Text hint for the field
   - `value` **optional** - the value of the field, can also be bound to (default: `''`)
 -->
 <div class="relative mb-14 sm:mb-8 md:col-span-2">
@@ -30,7 +32,9 @@
 		class="peer-placeholder-shown:text-blackcoffee-500/60; absolute left-4 top-0 text-xs font-semibold text-blackcoffee-500/70 transition-all duration-200 peer-placeholder-shown:top-[0.55rem] peer-placeholder-shown:text-base peer-placeholder-shown:font-normal"
 		>{label}</label
 	>
-	<div class="absolute w-full text-sm text-blackcoffee/70 peer-focus:text-blackcoffee/70">
-		Who will be taking part in the shoot? Include name, age, and any special accommodation info
-	</div>
+	{#if hint != ''}
+		<div class="absolute w-full text-sm text-blackcoffee/70 peer-focus:text-blackcoffee/70">
+			{hint}
+		</div>
+	{/if}
 </div>
